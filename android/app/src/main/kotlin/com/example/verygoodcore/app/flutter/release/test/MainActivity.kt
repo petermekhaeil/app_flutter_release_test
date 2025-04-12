531fd0f5e2bf365802049f0fea17b3832bfa64a7
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2025-04-12 15:20:34 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/app/flutter/release/test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.app_flutter_release_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:app_flutter_release_test/app/app.dart';
+import 'package:app_flutter_release_test/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
